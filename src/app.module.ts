bf0ff3561868403641eef6a9ee8394009ae3b73b
Juliette117 <ajuliette7@gmail.com>
2025-03-28 16:47:59 +0100

    Configuration de Postgres et Neo4j :wrench:

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,29 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { createConnection } from 'neo4j-driver';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+  ],
+  providers: [
+    {
+      provide: 'NEO4J',
+      useFactory: () => createConnection(process.env.NEO4J_URI, {
+        auth: { username: process.env.NEO4J_USER, password: process.env.NEO4J_PASSWORD },
+      }),
+    },
+  ],
 })
 export class AppModule {}
