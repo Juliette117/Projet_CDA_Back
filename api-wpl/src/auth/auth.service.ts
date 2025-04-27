9efcc6ded2f19bfddf7fad94f4c5d7759e8f5b73
Juliette117 <ajuliette7@gmail.com>
2025-04-27 21:30:33 +0200

    feat: Ajout fichiers auth :sparkles:

---
+++ api-wpl/src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UserService,
+    private jwtService: JwtService,
+  ) {}
+
+  async signup(email: string, username: string, password: string) {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    return this.usersService.create(email, username, hashedPassword);
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      throw new UnauthorizedException('Email ou mot de passe invalide');
+    }
+
+    const payload = { email: user.email, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
